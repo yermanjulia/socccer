3d9bdbdda5d9e4adc63ee376bcd03e8fbbf6bd7f
Julia Yerman <juliyaerm88@gmail.com>
2023-07-23 15:12:15 -0700

    Updated about, contactus and staff pages, code refactored, code cleaned up

--- app.js
+++ app.js
@@ -5,3 +5,25 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll(".slide");
+
+  slides[activeSlide].classList.add("active");
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+
+      slide.classList.add("active");
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
+}
+
+slidesPlugin(0);
